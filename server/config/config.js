58298d7b8ddc3c04b68015841ec7e6771b42b502
Fabrice <contact@fabricemaigne.com>
2019-11-11 17:10:16 +0100

    Variable de entorno

--- server/config/config.js
+++ server/config/config.js
@@ -9,7 +9,7 @@ let urlDB;
 if(process.env.NODE_ENV === 'dev'){
     urlDB = 'mongodb://localhost:27017/cafe';
 }else{
-    urlDB = 'mongodb+srv://fabriceontour:Z1IWyyIZfioyhFAD@cluster0-zsjhw.mongodb.net/cafe';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
